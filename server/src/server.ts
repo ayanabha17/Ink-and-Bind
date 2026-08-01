c008669bb9527c4ad4449f46e5de5ee10423ec2f
Ayanabha Ganguly <ayanabhaganguly99@gmail.com>
2026-08-01 14:31:48 +0530

    fix: implement Google DNS override to bypass ISP SRV blocking

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import mongoose from 'mongoose';
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
